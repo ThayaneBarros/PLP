202feb0e485628d5beb665174a25ce720e06e99a
Thayane Barros <thayanes438@gmail.com>
2023-04-14 19:28:45 -0300

    atualizando exercicio

--- PraticasFuncional/PraticaFuncional01/PF01.hs
+++ PraticasFuncional/PraticaFuncional01/PF01.hs
@@ -1,13 +1,24 @@
 import Text.Printf
 
-calculaImposto :: Float -> String
+calculaImposto :: Float -> IO ()
 calculaImposto valor 
-                    | valor <= 1903.98 = imprimiSaida valor valor
-                    | valor <= 2826.65 = imprimiSaida (142+0.075*(valor - 1903.98)) (valor - (142+0.075*(valor - 1903.98)))
-                    | valor <= 3751.05 = imprimiSaida (354.80+0.15*(valor - 2826.65)) (valor - (354.80+0.15*(valor - 2826.65)))
-                    | valor <= 4664.68 = imprimiSaida (636.13+0.225*(valor - 3751.05)) (valor - (636.13+0.225*(valor - 3751.05)))
-                    | otherwise = imprimiSaida (869.36 +0.275*(valor - 4664.68)) (valor - (869.36 +0.275*(valor - 4664.68)))
+                    | valor <= 1903.98 = putStrLn(imprimiSaida impostoRenda salarioLiquido)
+                    | valor <= 2826.65 = putStrLn(imprimiSaida impostoRenda salarioLiquido)
+                    | valor <= 3751.05 = putStrLn(imprimiSaida impostoRenda salarioLiquido)
+                    | valor <= 4664.68 = putStrLn(imprimiSaida impostoRenda salarioLiquido)
+                    | otherwise = putStrLn(imprimiSaida impostoRenda salarioLiquido)
+                    where impostoRenda = calculaImpostoRetido valor
+                          salarioLiquido = valor - impostoRenda
+
+calculaImpostoRetido :: Float -> Float
+calculaImpostoRetido valor
+                    | valor <= 1903.98 = 1903.98
+                    | valor <= 2826.65 = 142+0.075*(valor - 1903.98)
+                    | valor <= 3751.05 = 354.80+0.15*(valor - 2826.65)
+                    | valor <= 4664.68 = 636.13+0.225*(valor - 3751.05)
+                    | otherwise = 869.36 +0.275*(valor - 4664.68)
 
 imprimiSaida :: Float -> Float -> String
-imprimiSaida valorImposto valorSalario = printf "%s %.2f" "O valor do imposto de renda retido eh: R$ " valorImposto 
-                                         printf "%s %.2f" "O salario liquido eh: R$ " valorSalario
+imprimiSaida impostoRenda salarioLiquido =
+  printf "O valor do imposto de renda retido eh: R$ %.2f\n" impostoRenda ++
+  printf "O salario liquido eh: R$ %.2f" salarioLiquido
